6444e02b6c547689584e17d56b53394e6db489c3
Eva <jialingtsai@gmail.com>
2025-05-24 10:59:11 +0800

    modify

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,16 +1,16 @@
-def caculate_bmi(h:int, w:int)->float:
-   return  w / (h /100) ** 2
+def caculate_bmi(height:int, weight:int)->float:
+   return  weight / (height /100) ** 2
 
-def get_state(b:float) ->str:
-   if b < 18.5:
+def get_state(bmi:float) ->str:
+   if bmi < 18.5:
       return "體重過輕"
-   elif b < 24:
+   elif bmi < 24:
       return "正常範圍"
-   elif b < 27:
+   elif bmi < 27:
       return "過重"
-   elif b < 30:
+   elif bmi < 30:
       return "輕度肥胖"
-   elif b < 35:
+   elif bmi < 35:
       return "中度肥胖"
    else 
       return "重度肥胖"
