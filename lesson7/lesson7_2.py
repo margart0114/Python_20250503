ef999648c5afe50db8ebd78cb2cc2eaf90ba1f82
Eva <jialingtsai@gmail.com>
2025-05-24 11:17:31 +0800

    modify

--- lesson7/lesson7_2.py
+++ lesson7/lesson7_2.py
@@ -15,11 +15,14 @@ def get_state(bmi:float)->str:
     else:
         return "重度肥胖"
 
-height:int = int(input("請輸入身高(cm):"))
-weight:int = int(input("請輸入體重(kg):"))
+def main():
+    height:int = int(input("請輸入身高(cm):"))
+    weight:int = int(input("請輸入體重(kg):"))
+    bmi = caculate_bmi(height, weight)
 
-bmi = caculate_bmi(height, weight)
+    print(bmi)
+    print(get_state(bmi))
 
-print(bmi)
-print(get_state(bmi))
+if __name__ == "__main__":
+    main()
 
