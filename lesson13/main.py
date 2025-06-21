1394f76bf488cdd76d53dc1d720601f82adbcb2b
Eva <jialingtsai@gmail.com>
2025-06-21 16:24:42 +0800

    modified

--- lesson13/main.py
+++ lesson13/main.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 def main():
     st.title("My Streamlit App")
     st.write("Hello, Streamlit!")
+
+    #Load the CSV file
+    df = pd.read_csv("taiwan.csv")
+
+    #Display the DataFrame  
+    st.write("以下是"taiwan.csv"的內容:")   
+    st.DataFrame(df)
+    
 
 if __name__=="__main__":
     main()
