983b13c5ec7116272fd3339b23ce351bda2c3353
Eva <jialingtsai@gmail.com>
2025-06-21 16:00:43 +0800

    modified

--- lesson13/main.py
+++ lesson13/main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 def main():
-
-st.title("My Streamlit App")
-st.write("Hello, Streamlit!")
+    st.title("My Streamlit App")
+    st.write("Hello, Streamlit!")
 
 if __name__=="__main__":
     main()
