33f411e92b14fa12d341269727d534b1697edd5a
Eva <jialingtsai@gmail.com>
2025-06-28 13:52:55 +0800

    modified

--- lesson15/lesson15_1.py
+++ lesson15/lesson15_1.py
@@ -1,9 +1,16 @@
 import yfinance as yf
-tw2330 = yf.download('2330.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
-tw2303 = yf.download('2303.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
-tw2454 = yf.download('2454.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
-tw2317 = yf.download('2317.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
+def download_data():
+    """
+    1.下載yfinance的4檔股票資料,股票有: 2330.TW,2303.TW,2454.TW,2317.TW
+    2. 在目前目錄下
+    """
+    tw2330 = yf.download('2330.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
+    tw2303 = yf.download('2303.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
+    tw2454 = yf.download('2454.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
+    tw2317 = yf.download('2317.TW',start='2024-01-01',end='2024-06-01',auto_adjust=True)
 
+def main():
+    download_data()
 
-
-__name__ == '__main__'
+if __name__ == '__main__':
+    main()
